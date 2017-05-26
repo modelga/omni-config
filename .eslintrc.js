16c0302302eff04006b92c8b63ab158653c6712d
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2017-05-26 11:01:56 +0200

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: false,
+    node: true
+  },
+  extends: 'airbnb-base',
+  // add your custom rules here
+  'rules': {
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
